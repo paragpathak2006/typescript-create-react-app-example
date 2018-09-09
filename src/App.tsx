a9e3113d45a2dd799af9063d79a9b965e35c65d9
Robert S <rsavian@nerdery.com>
2018-09-08 22:16:03 -0500

    initial format

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,24 @@ import './App.css';
 
 import logo from './logo.svg';
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+export default class App extends React.Component {
 
-export default App;
+    public render(): JSX.Element {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img
+                        src={logo}
+                        className="App-logo"
+                        alt="logo"
+                    />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.tsx</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+
+}
