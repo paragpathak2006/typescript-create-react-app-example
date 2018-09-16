906f4e6094d312f037d22f0566626944c6e11e1f
Robert S <rsavian@nerdery.com>
2018-09-16 16:57:49 -0500

    update types

--- src/stores/topics/ContentService.ts
+++ src/stores/topics/ContentService.ts
@@ -1,10 +1,11 @@
 import HttpUtility from '../../utilities/HttpUtility';
+import ITopic from './models/ITopic';
 
 export default class ContentService {
 
     private static _http: HttpUtility = new HttpUtility();
 
-    public static async loadContent(): Promise<any> {
+    public static async loadContent(): Promise<ITopic[]> {
         const endpoint: string = '/data/content.json';
 
         return ContentService._http.get(endpoint);
