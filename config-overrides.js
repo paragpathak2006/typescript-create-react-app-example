6ae3430a1e658f632ee81ccb3ced32dd94e81516
Robert S <rsavian@nerdery.com>
2019-02-15 18:33:20 -0600

    add customize-cra to fix cra 2.0 react-app-rewired

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,8 @@
 const path = require('path');
+const {override, addWebpackAlias} = require('customize-cra');
 
-module.exports = function override(config, env) {
-    // Logic to load either the src/environments/production or src/environments/staging file in the app.
-    config.resolve.alias = Object.assign({}, config.resolve.alias, {
-        environment: path.join(__dirname, 'src', 'environments', env)
-    });
-
-    return config;
-};
+module.exports = override(
+    addWebpackAlias({
+        environment: path.join(__dirname, 'src', 'environments', process.env.NODE_ENV)
+    })
+);
